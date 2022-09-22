1b2b8dcf8794577a3a2f6782e5664fad7e86c008
Anilatta <95362042+Anilatta@users.noreply.github.com>
2022-09-22 15:55:07 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import { Audio } from "react-loader-spinner";
+
 export default function Weather(props) {
   function handleSubmit(response) {
     alert(
@@ -13,14 +13,5 @@ export default function Weather(props) {
   let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=9583d26359a57772b18997e04cbea4af&units=metric`;
   axios.get(url).then(handleSubmit);
 
-  return <h2>Hello From Weather</h2>;
-  <Audio
-    height="80"
-    width="80"
-    radius="9"
-    color="green"
-    ariaLabel="loading"
-    wrapperStyle
-    wrapperClass
-  />;
+  return <h1>Hello</h1>;
 }
