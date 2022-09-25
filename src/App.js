3185744282cd4adfd10ea7f4ef4f67502b28c676
Anilatta <95362042+Anilatta@users.noreply.github.com>
2022-09-25 16:37:25 +0200

    Add the project template

--- src/App.js
+++ src/App.js
@@ -3,27 +3,22 @@ import "./App.css";
 
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-        <Weather city="Paris" />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <Weather defaultCity="Kyiv" />
+        <footer>
+          This project was coded by Talina Dzhavakhadze and is
+          <a
+            href="https://github.com/Anilatta/vanilla-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -1,17 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-
-export default function Weather(props) {
-  function handleSubmit(response) {
-    alert(
-      `Weather in ${response.data.name} is ${Math.round(
-        response.data.main.temp
-      )}`
-    );
-  }
-
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=9583d26359a57772b18997e04cbea4af&units=metric`;
-  axios.get(url).then(handleSubmit);
-
-  return <h1>Hello</h1>;
+export default function Weather() {
+  return (
+    <div id="root">
+      <div className="App">
+        <div className="container">
+          <div className="Weather">
+            <form id="search-form">
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    type="search"
+                    placeholder="Enter a city.."
+                    className="form-control search-input"
+                    id="city-input"
+                    autocomplete="off"
+                  />
+                </div>
+                <div className="col-3 p-0">
+                  <input
+                    type="submit"
+                    className="btn btn-primary w-100"
+                    value="Search"
+                  />
+                </div>
+              </div>
+            </form>
+            <div className="WeatherInfo">
+              <div className="row">
+                <div className="col-6">
+                  <h1 id="city">New York</h1>
+                  <ul>
+                    <li>
+                      Last updated:
+                      <span id="date">Monday 09:49</span>,
+                      <div id="description">clear sky</div>
+                    </li>
+                    <li>
+                      Humidity:{" "}
+                      <strong>
+                        <span id="humidity">48</span>%
+                      </strong>
+                      , Wind:
+                      <strong>
+                        <span id="wind">4.12</span>m/s
+                      </strong>
+                    </li>
+                  </ul>
+                </div>
+                <div className="col-6">
+                  <div className="temperature-container d-flex justify-content-end">
+                    <canvas width="52" height="52"></canvas>
+                    <img src="" alt="Clear" id="icon" className="float-left" />
+                    <div>
+                      <span className="temperature" id="temperature">
+                        20
+                      </span>
+                      <span className="unit">
+                        <a href="#" id="celcius-link" className="active">
+                          °C
+                        </a>
+                        |
+                        <a href="#" id="fahrenheit-link">
+                          °F
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="weather-forecast" id="forecast"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
